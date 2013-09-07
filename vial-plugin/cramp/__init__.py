639af063f914d85cea3945a48f535e10c342462e
Anton Bobrov <bobrov@vl.ru>
2013-09-07 21:55:42 +0400

    skip bracket by the same char

--- vial-plugin/cramp/__init__.py
+++ vial-plugin/cramp/__init__.py
@@ -14,7 +14,9 @@ OPEN2CLOSE = dict(BRACKETS)
 tail = []
 
 def init():
-    register_function('<SID>VialCrampClose(what)',  close)
+    register_function('<SID>VialCrampClose()',  close)
+    register_function('<SID>VialCrampOpen(end)',  open)
+    register_function('<SID>VialCrampOpenClose(start)',  open_close)
     register_function('<SID>VialCrampLeave()', leave)
     register_function('<SID>VialCrampSkip()', skip)
     register_function('<SID>VialCrampBS()', backspace)
@@ -25,17 +27,63 @@ def init():
     vim.command('inoremap <bs> <c-r>=<SID>VialCrampBS()<cr><bs>')
 
     for s, e in BRACKETS:
-        if e == '"':
-            e = '\\"'
+        ss = s.replace('"', '\\"')
+        ee = e.replace('"', '\\"')
 
-        vim.command('inoremap {0} {0}<c-r>=<SID>VialCrampClose("{1}")<cr>'.format(s, e))
+        if s == e:
+            vim.command('inoremap {0} {0}<c-r>=<SID>VialCrampOpenClose("{1}")<cr>'.format(s, ss))
+        else:
+            vim.command('inoremap {0} {0}<c-r>=<SID>VialCrampOpen("{1}")<cr>'.format(s, ee))
+            vim.command('inoremap {0} {0}<c-r>=<SID>VialCrampClose()<cr>'.format(e))
 
+
+def modify_current_line(left, mid, right):
+    l = vim.current.line
+    vim.current.line = l[:left] + mid + l[right:]
 
 @vimfunction
-def close(what):
-    tail.insert(0, what)
+def close():
     col = vim.current.window.cursor[1]
-    vim.current.line = vim.current.line[:col] + what + vim.current.line[col:]
+    if col >= 1:
+        try:
+            end = vim.current.line[col-1]
+            rchar = vim.current.line[col]
+        except IndexError:
+            pass
+        else:
+            if rchar == end:
+                if tail and tail[0] == end:
+                    tail.pop(0)
+
+                modify_current_line(col, '', col + len(end))
+
+    return ''
+
+@vimfunction
+def open_close(start):
+    col = vim.current.window.cursor[1]
+
+    try:
+        rchar = vim.current.line[col]
+    except IndexError:
+        pass
+    else:
+        if rchar == start:
+            if tail and tail[0] == start:
+                tail.pop(0)
+
+            modify_current_line(col, '', col + len(start))
+            return ''
+
+    tail.insert(0, start)
+    modify_current_line(col, start, col)
+    return ''
+
+@vimfunction
+def open(end):
+    tail.insert(0, end)
+    col = vim.current.window.cursor[1]
+    modify_current_line(col, end, col)
     return ''
 
 @vimfunction
@@ -44,7 +92,7 @@ def leave():
         return ''
 
     col = vim.current.window.cursor[1]
-    vim.current.line = vim.current.line[:col] + vim.current.line[col+len(tail):]
+    modify_current_line(col, '', col + len(tail))
 
     result = ''.join(tail)
     tail[:] = []
@@ -55,7 +103,7 @@ def leave():
 def skip():
     col = vim.current.window.cursor[1]
     char = vim.current.line[col]
-    vim.current.line = vim.current.line[:col] + vim.current.line[col+1:]
+    modify_current_line(col, '', col + 1)
 
     if tail and tail[0] == char:
         tail.pop(0)
@@ -73,7 +121,7 @@ def backspace():
         return ''
 
     if tail and lchar in OPEN2CLOSE and OPEN2CLOSE[lchar] == rchar and rchar == tail[0]:
-        vim.current.line = vim.current.line[:col] + vim.current.line[col+1:]
+        modify_current_line(col, '', col + 1)
         tail.pop(0)
 
     return ''
